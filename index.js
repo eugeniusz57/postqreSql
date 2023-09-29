9300ef38dd23b68fece46e67f26e0534b08284c8
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-09-29 17:18:37 +0200

    create tabels

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("Hello post");
 });
 
 app.listen(PORT, () => console.log(`server started on post ${PORT}`));
