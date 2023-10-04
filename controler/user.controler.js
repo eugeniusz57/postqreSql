121174007a5a9d3effa39cef4423ecbc58d16405
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-10-04 17:38:53 +0200

    add new person in db

--- controler/user.controler.js
+++ controler/user.controler.js
@@ -1,11 +1,16 @@
+const db = require('../db')
+
 class UserController {
   async createUser(req, res) {
     const { name, surname } = req.borderStyle;
+    const newPerson = await db.query('INSERT INTO person (name, surname) values ($1, $2) RETURNING *', [name, surname])
     console.log(name, surname);
-    res.json("ok");
+    res.json(newPerson);
   }
 
-  async getUser(req, res) {}
+  async getUser(req, res) {
+
+  }
 
   async getOneUser(req, res) {}
 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(express.json())
 app.use('/api', userRouter )
 
 app.listen(PORT, () => console.log(`server started on post ${PORT}`));
