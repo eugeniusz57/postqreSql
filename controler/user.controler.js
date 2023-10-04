d6e00bd296d32b00d4fc5fdfdd123619a5d67e13
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-10-04 17:33:01 +0200

    create create user

--- controler/user.controler.js
+++ controler/user.controler.js
@@ -1,24 +1,17 @@
-class UserController{
-    async createUser(req, res){
+class UserController {
+  async createUser(req, res) {
+    const { name, surname } = req.borderStyle;
+    console.log(name, surname);
+    res.json("ok");
+  }
 
-    }
+  async getUser(req, res) {}
 
-    async getUser(req, res){
-        
-    }
+  async getOneUser(req, res) {}
 
-    async getOneUser(req, res){
-        
-    }
+  async updateUser(req, res) {}
 
-    async updateUser(req, res){
-        
-    }
-    
-    async deleteUser(req, res){
-        
-    }
+  async deleteUser(req, res) {}
 }
 
-module.export = new UserController()
-
+module.export = new UserController();
